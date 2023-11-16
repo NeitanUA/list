2eafb15b26925831694bc0438e53541e82815655
NeitanUA <natanael1523@gmail.com>
2023-11-16 20:05:17 -0300

    properties, Game, ORM

--- src/main/java/com/natanael/list/ListApplication.java
+++ src/main/java/com/natanael/list/ListApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ListApplication {
-	
+
 	public static void main(String[] args) {
-		SpringApplication.run(ListApplication.class, args);		
-	}	
+		SpringApplication.run(ListApplication.class, args);
+	}
+
 }
